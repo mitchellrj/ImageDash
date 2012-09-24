5df0ab94e1f9571976c5c0f8eab57e5cbc82018b
Richard Mitchell <richard.j.mitchell@gmail.com>
2012-09-24 09:31:25 +0100

    Comments

--- browser.js
+++ browser.js
@@ -38,17 +38,20 @@
             ev.preventDefault();
             var img1 = document.getElementById('file1');
             readFiles(img1.files, function (img1File) {
+                // first image read, display
                 var img2 = document.getElementById('file2'),
                     img1El = new Image(),
                     dataUrl = img1File.data;
                 img1El.onload = function () {
+                    // first image loaded, start loading second image
                     readFiles(img2.files, function (img2File) {
+                        // second image read, display
                         var img2El = new Image(),
                             dataUrl = img2File.data,
-                            id = new ImageDash(64, 10),
-                            comparisonResult;
+                            id = new ImageDash(64, 10);
                         img2El.onload = function () {
-                            comparisonResult = id.compareImages(img1El, img2El);
+                            // second image loaded, compare images
+                            var comparisonResult = id.compareImages(img1El, img2El);
                             document.getElementById('result').innerHTML = ((1 - comparisonResult / 4) * 100).toString() + '%';
                         };
                         img2El.src = dataUrl;
